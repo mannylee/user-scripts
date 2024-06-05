8b5a326237e7c1e88a4247ca3dc63883649ed353
mannylee <manny.mf.lee@gmail.com>
2024-06-05 11:50:13 +0800

    Create cathay-pacific-scroll-fixer.user.js

---
+++ user-scripts/cathay-pacific-scroll-fixer.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Cathay Pacific Scroll Fixer
+// @namespace    https://github.com/mannylee
+// @version      2024.06.05
+// @description  Fix forms on the Cathay Pacific site
+// @author       mannylee
+// @match        https://www.cathaypacific.com/*
+// @icon         https://github.githubassets.com/favicons/favicon-dark.png
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+
+
+(function() {
+    'use strict';
+
+    $("body").css("height", "100vh");
+    $("body").css("overflow", "auto");
+
+})();
